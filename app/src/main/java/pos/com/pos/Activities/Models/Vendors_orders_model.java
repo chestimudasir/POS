df4067df305782998626cd9da8bbffd3d6129a01
zeeshanmir <zeeshanmir200@gmail.com>
2018-10-24 21:46:47 +0530

    Remove un used UI files for deceased storage use.
    Signed-off-by: hardmode98 <zeeshanmir200@gmail.com>

---
+++ app/src/main/java/pos/com/pos/Activities/Models/Vendors_orders_model.java
@@ -0,0 +1,11 @@
+package pos.com.pos.Activities.Models;
+
+import java.util.ArrayList;
+
+public class Vendors_orders_model {
+
+    public String uid_vendor , ordered_at;
+    public byte priority;
+    public ArrayList<String> items;
+
+}
